6e95863592153fc35c3748b6cbde956ed50de79e
root <root@vultr.guest>
2023-07-14 21:19:03 +0000

    latest broken

--- bot_main.py
+++ bot_main.py
@@ -12,24 +12,19 @@ bot_thread = None
 crm_api = CRMAPI()
 
 def parse_wall_height(message):
-    # Use regex to find a digit (2, 3, or 4) in the message
     match = re.search(r"\b[234]\b", message)
     return match.group(0) if match else None
 
 def select_template(lead_data, templates):
-    # Extract lead preferences from custom fields or notes
     if 'Custom_Data' in lead_data:
         preferred_hitch_type = lead_data['Custom_Data'].get('Preferred_hitch_type')
         trailer_size = lead_data['Custom_Data'].get('What_size_dump_trailer_do_you_need')
     else:
-        # Extract preferences from notes (assuming the first note contains the necessary information)
         note_content = lead_data['notes'][0]['note']
         trailer_size, preferred_hitch_type = note_content.split()
 
-    # Generate template title based on lead preferences
     template_title = f"{trailer_size}{' Gooseneck' if preferred_hitch_type == 'Gooseneck' else ''}"
 
-    # Return the template with the matching title
     for template in templates:
         if template['name'] == template_title:
             return template
@@ -51,11 +46,12 @@ def run_bot():
                 try:
                     lead_id = task['lead_id']
                     lead_data = crm_api.get_lead_data(lead_id)
-                    # Get lead notes
+                    if lead_data is None:
+                        logging.error(f"Failed to get lead data for lead {lead_id}")
+                        continue
                     lead_data['notes'] = crm_api.get_lead_notes(lead_id)
                     if 'last_received_message' in lead_data:
                         wall_height = parse_wall_height(lead_data['last_received_message']['text'])
-                        # Update the template selection logic
                         template = select_template(lead_data, templates)
                         if template and wall_height in template['text']:
                             message = template['text'].replace('{{ wall_height }}', wall_height)

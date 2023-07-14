03167c2623811a1a7cef018d97111523d2df8943
root <root@vultr.guest>
2023-07-14 19:28:35 +0000

    latest running but not sending sms nor updating status

--- bot_main.py
+++ bot_main.py
@@ -1,3 +1,4 @@
+# 'bot_main.py'
 import time
 from threading import Thread
 from flask import Flask, jsonify
@@ -11,59 +12,71 @@ app = Flask(__name__)
 bot_thread = None
 crm_api = CRMAPI()
 
-def find_template(lead_data, templates):
-    ...
-    # (This function remains unchanged)
-    ...
+def parse_wall_height(message):
+    # Use regex to find a digit (2, 3, or 4) in the message
+    match = re.search(r"\b[234]\b", message)
+    return match.group(0) if match else None
 
-def run_bot(max_leads_per_run=10):
+def select_template(lead_data, templates):
+    # Extract lead preferences from custom fields or notes
+    if 'Custom_Data' in lead_data:
+        preferred_hitch_type = lead_data['Custom_Data'].get('Preferred_hitch_type')
+        trailer_size = lead_data['Custom_Data'].get('What_size_dump_trailer_do_you_need')
+    else:
+        # Extract preferences from notes (assuming the first note contains the necessary information)
+        note_content = lead_data['notes'][0]['note']
+        trailer_size, preferred_hitch_type = note_content.split()
+
+    # Generate template title based on lead preferences
+    template_title = f"{trailer_size}{' Gooseneck' if preferred_hitch_type == 'Gooseneck' else ''}"
+
+    # Return the template with the matching title
+    for template in templates:
+        if template['name'] == template_title:
+            return template
+
+    return None
+
+def run_bot():
     logging.basicConfig(filename='app.log', level=logging.INFO)
 
     while True:
         try:
+            tasks = crm_api.get_unprocessed_incoming_sms_tasks()
+            templates = crm_api.get_sms_templates()
             sent_counter = 0
             human_intervention_counter = 0
-            fail_counter = 0
-
-            # Get unprocessed tasks of type 'incoming_sms'
-            tasks = crm_api.get_unprocessed_incoming_sms_tasks()[:max_leads_per_run]
+            failed_counter = 0
             for task in tasks:
-                lead_id = task['lead_id']
-
-                if lead_id is None:
-                    logging.warning("Skipping task with None lead_id.")
-                    continue
-
-                lead_data = crm_api.get_lead_data(lead_id)
-                if not lead_data or 'last_received_message' not in lead_data:
-                    fail_counter += 1
-                    continue
-
-                response_text = generate_response(lead_data['last_received_message']['text'])
-
-                template = find_template(lead_data, templates)
-                if template:
-                    crm_api.send_message(lead_id, template['text'], CRM_PHONE_NUMBER)
-                    sent_counter += 1
-                else:
-                    crm_api.update_lead_status(lead_id, 'Human Intervention')
-                    human_intervention_counter += 1
-
-                # Mark the task as complete
-                crm_api.mark_task_as_complete(task['id'])
-
-            print(f"Successfully sent messages to {sent_counter} leads.")
-            print(f"Marked {human_intervention_counter} leads for human intervention.")
-            print(f"Failed to process {fail_counter} leads.")
-            time.sleep(5)
-
+                try:
+                    lead_id = task['lead_id']
+                    lead_data = crm_api.get_lead_data(lead_id)
+                    # Get lead notes
+                    lead_data['notes'] = crm_api.get_lead_notes(lead_id)
+                    if 'last_received_message' in lead_data:
+                        wall_height = parse_wall_height(lead_data['last_received_message']['text'])
+                        # Update the template selection logic
+                        template = select_template(lead_data, templates)
+                        if template and wall_height in template['text']:
+                            message = template['text'].replace('{{ wall_height }}', wall_height)
+                            crm_api.send_message(lead_id, message, task['id'])
+                            sent_counter += 1
+                        else:
+                            crm_api.update_lead_status(lead_id, 'Human Intervention')
+                            human_intervention_counter += 1
+                    crm_api.mark_task_as_complete(task['id'])
+                except Exception as e:
+                    logging.exception(f"Failed to process lead {lead_id}")
+                    failed_counter += 1
+            logging.info(f"Sent {sent_counter} messages, marked {human_intervention_counter} leads for human intervention, failed to process {failed_counter} leads")
         except Exception as e:
-            logging.error(f"Error in run_bot: {str(e)}")
+            logging.exception("Failed to fetch tasks")
+        time.sleep(5)
 
 @app.route('/start', methods=['POST'])
 def start_bot():
     global bot_thread
-    if bot_thread is None:
+    if bot_thread is None or not bot_thread.is_alive():
         bot_thread = Thread(target=run_bot)
         bot_thread.start()
     return jsonify(success=True)
@@ -71,10 +84,14 @@ def start_bot():
 @app.route('/stop', methods=['POST'])
 def stop_bot():
     global bot_thread
-    if bot_thread is not None:
-        bot_thread.join()
+    if bot_thread is not None and bot_thread.is_alive():
         bot_thread = None
     return jsonify(success=True)
 
+@app.route('/logs', methods=['GET'])
+def get_logs():
+    with open('app.log', 'r') as f:
+        return f.read()
+
 if __name__ == "__main__":
-    app.run(port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000)
--- crm_api.py
+++ crm_api.py
@@ -29,6 +29,15 @@ class CRMAPI:
             logging.error(f"Failed to get lead data for lead_id {lead_id}: {response.text}")
             return None
 
+    def get_lead_notes(self, lead_id):
+        url = f'{CRM_API_URL}/activity/note/?lead_id={lead_id}'
+        response = requests.get(url, auth=self.auth)
+        if response.status_code == 200:
+            return response.json()['data']
+        else:
+            logging.error(f"Failed to get lead notes for lead_id {lead_id}: {response.text}")
+            return None
+
     def send_message(self, lead_id, message, message_id):
         url = f'{CRM_API_URL}/activity/sms'
         data = {
@@ -39,7 +48,11 @@ class CRMAPI:
             'related_to': message_id
         }
         response = requests.post(url, json=data, auth=self.auth)
-        return response.status_code == 201
+        if response.status_code == 201:
+            return True
+        else:
+            logging.error(f"Failed to send message for lead_id {lead_id}: {response.text}")
+            return False
 
     def mark_task_as_complete(self, task_id):
         url = f'{CRM_API_URL}/task/{task_id}'
@@ -47,7 +60,11 @@ class CRMAPI:
             'is_complete': True
         }
         response = requests.put(url, json=data, auth=self.auth)
-        return response.status_code == 200
+        if response.status_code == 200:
+            return True
+        else:
+            logging.error(f"Failed to mark task as complete for task_id {task_id}: {response.text}")
+            return False
 
     def update_lead_status(self, lead_id, status):
         url = f'{CRM_API_URL}/lead/{lead_id}'
@@ -55,4 +72,17 @@ class CRMAPI:
             'status_label': status
         }
         response = requests.put(url, json=data, auth=self.auth)
-        return response.status_code == 200
+        if response.status_code == 200:
+            return True
+        else:
+            logging.error(f"Failed to update lead status for lead_id {lead_id}: {response.text}")
+            return False
+
+    def get_sms_templates(self):
+        url = f'{CRM_API_URL}/sms_template/'
+        response = requests.get(url, auth=self.auth)
+        if response.status_code == 200:
+            return response.json()['data']
+        else:
+            logging.error(f"Failed to get SMS templates: {response.text}")
+            return None

99ecdbbfd8c1a579cb03f4b71a4874395d31dab7
root <root@vultr.guest>
2023-07-14 11:55:39 +0000

    latest not sending sms or status change

--- bot_main.py
+++ bot_main.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 bot_thread = None
 crm_api = CRMAPI()
 
+
 def find_template(lead_data, templates):
     # Extract the necessary fields from the lead data
     hitch_type = lead_data['custom_fields'].get('preferred_hitch_type', '')
@@ -33,17 +34,24 @@ def find_template(lead_data, templates):
     # If no matching template is found, return None
     return None
 
-def run_bot():
+
+def run_bot(max_leads_per_run=10):
     while True:
+        # Initialize counters
+        sent_counter = 0
+        human_intervention_counter = 0
+        fail_counter = 0
+
         # Get unread messages
-        tasks = crm_api.get_unread_messages()
+        tasks = crm_api.get_unread_messages()[:max_leads_per_run]
         for task in tasks:
             lead_id = task['lead_id']
             message_id = task['id']
 
             # Get lead data
             lead_data = crm_api.get_lead_data(lead_id)
-            if not lead_data:
+            if not lead_data or 'last_received_message' not in lead_data:
+                fail_counter += 1
                 continue
 
             # Generate a response using the OpenAI API
@@ -54,12 +62,18 @@ def run_bot():
             if template:
                 # If a matching template is found, use it to send a response
                 crm_api.send_message(lead_id, template['text'], message_id)
+                sent_counter += 1
             else:
                 # If no matching template is found, mark the lead for human intervention
                 crm_api.update_lead_status(lead_id, 'Human Intervention')
+                human_intervention_counter += 1
 
             # Mark the task as complete
             crm_api.mark_task_as_complete(task['id'])
+
+        print(f"Successfully sent messages to {sent_counter} leads.")
+        print(f"Marked {human_intervention_counter} leads for human intervention.")
+        print(f"Failed to process {fail_counter} leads.")
 
         time.sleep(5)
 
